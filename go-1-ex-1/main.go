ef7bb2271ca59b944c993fc87d02a0d055e1e67c
ZEHRA NUHIU <zehra_nuhiu@sluz.ch>
2025-11-21 15:02:44 +0100

    number 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	firstName := "Zehra"
+	lastName := "Nuhiu"
+	dayOfBirth := 26
+	monthOfBirth := 6
+	yearOfBirth := 2007
+	numberOfSiblings := 2
+	heightInMeters := 1.68
+	zodiacSign := '\u264B'
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
